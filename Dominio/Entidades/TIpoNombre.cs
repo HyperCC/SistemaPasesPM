31a3c6a860022f86ada0ec8a4f25a2bd8afeace1
HyperCC <charliecondorcet@gmail.com>
2021-04-22 16:45:42 -0400

    feat(d-tipoNombre): add entidad TipoPersona

---
+++ Dominio/Entidades/TIpoNombre.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dominio.Entidades
+{
+    public class TIpoNombre
+    {
+        // atributos de la entidad
+        public string Nombre { get; set; }
+        public string Tipo { get; set; } // nombre o apellido
+        public string Posicion { get; set; } // primero, segundo, tercero.
+
+        // relacionamiento segun modelo R
+
+    }
+}
