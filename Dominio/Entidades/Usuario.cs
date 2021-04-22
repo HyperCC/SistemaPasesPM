90a4e9e44c65ef10733382fc4a61df3531b8c2cb
HyperCC <charliecondorcet@gmail.com>
2021-04-22 16:39:14 -0400

    feat(d-usuario): add entidad Usuario

---
+++ Dominio/Entidades/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dominio.Entidades
+{
+    public class Usuario
+    {
+        // atributos simples
+        public string Correo { get; set; }
+        public string Contrasena { get; set; }
+
+        // relacionamiento segun modelo R
+
+    }
+}
