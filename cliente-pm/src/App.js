bb76e1ced76f6dc3eadd8a8255bc4b08f37535b0
HyperCC <charliecondorcet@gmail.com>
2021-04-27 18:35:45 -0400

    chore(c-tailwind): add libreria tailwind

--- cliente-pm/src/App.js
+++ cliente-pm/src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className="bg-blue-300 text-red-500 p-3 text-xl font-bold text-center" >Si me funciono</p>
     </div>
   );
 }
