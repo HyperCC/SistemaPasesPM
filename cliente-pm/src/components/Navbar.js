9d685b8ace217c642b04cc6d62c25393d7c2ec6f
bastianMO <bastianMO@hotmail.com>
2021-04-28 15:14:32 -0400

    fix(c-Imagenes):Update solucion subida imagenes

--- cliente-pm/src/components/Navbar.js
+++ cliente-pm/src/components/Navbar.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Image from '../images/Blanco.png';
 
 const Navbar = () => {
     return(
         <nav className="flex justify-between items-center p-4 bg-blue-900">
-            <p class="text-2xl font-bold text-grey-800 text-white">Sistemas Pases de Accesos</p>
+            <img src={Image} alt="website logo" className="h-12 rounded ab-20 shadow" />
+            <p class="text-center text-2xl font-bold text-grey-800 text-white">Sistemas Pases de Accesos</p>
             <div className="flex space-x-2">
                 <a href="/"
                 className=" rounded bg-green-500 text-white p-2 px-4">
