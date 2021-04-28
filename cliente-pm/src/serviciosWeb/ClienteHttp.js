f0ead70f10a2e0dc83e332491f5b58a1d193ceba
HyperCC <charliecondorcet@gmail.com>
2021-04-28 12:22:45 -0400

    feat(c-clienteWeb): add uso de axios para consumir api

---
+++ cliente-pm/src/serviciosWeb/ClienteHttp.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:5000/api';
+
+// respuestas para verbos http con axios
+const requestGenerico = {
+    get: (url) => axios.get(url),
+    post: (url, body) => axios.post(url, body),
+    put: (url, body) => axios.put(url, body),
+    delete: (url) => axios.delete(url)
+};
+
+export default requestGenerico;
