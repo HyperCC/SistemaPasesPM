60d72084be00126f99e1bb661afaccf36dddd4c6
HyperCC <charliecondorcet@gmail.com>
2021-04-28 12:12:55 -0400

    feat(c-login): add login components

---
+++ cliente-pm/src/components/Login/CambiarClave.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function CambiarClave(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default CambiarClave;
---
+++ cliente-pm/src/components/Login/RecuperarClave.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function RecuperarClave(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default RecuperarClave;
