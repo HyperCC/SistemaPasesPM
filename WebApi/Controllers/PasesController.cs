f29bd8f1252d1e5430b5ab80182e8c5798e23267
Matías Quezada <matquez@gmail.com>
2021-04-29 13:31:33 -0400

    feat(w-paseVisita): add controlador de Pase

---
+++ WebApi/Controllers/PasesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Aplicacion.Pases;
+using Microsoft.AspNetCore.Mvc;
+using Dominio.Entidades;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Controllers.ControllerPersonalizado;
+
+namespace WebApi.Controllers
+{
+    public class PasesController : PersonalController
+    {
+        [HttpPost("registrar")]
+        public async Task<ActionResult<Pase>> Registrar(NuevoPaseVisita.Ejecuta parametros)
+        {
+            return await this.MediadorHerencia.Send(parametros);
+        }
+
+    }
+}
