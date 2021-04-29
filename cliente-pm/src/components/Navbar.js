e1e3b9024e2ff88fa3b4acc526eb84016d3f4b32
bastianMO <bastianMO@hotmail.com>
2021-04-29 17:55:32 -0400

    fix(c-color):update colores

--- cliente-pm/src/components/Navbar.js
+++ cliente-pm/src/components/Navbar.js
@@ -3,16 +3,16 @@ import Image from '../images/Blanco.png';
 
 const Navbar = () => {
     return(
-        <nav className="flex justify-between items-center p-4 bg-blue-900">
+        <nav className="flex justify-between items-center p-4 bg-azul-pm">
             <img src={Image} alt="website logo" className="h-12 rounded ab-20 shadow" />
             <p class="text-center text-2xl font-bold text-grey-800 text-white">Sistemas Pases de Accesos</p>
             <div className="flex space-x-2">
                 <a href="/"
-                className=" rounded bg-green-500 text-white p-2 px-4 hover:bg-yellow-400">
+                className=" rounded bg-verde-pm text-white p-2 px-4 hover:bg-amarillo-pm">
                     Perfil
                 </a>
                 <a href="/"
-                className=" rounded bg-green-500 text-white p-2 px-4 hover:bg-yellow-400">
+                className=" rounded bg-verde-pm text-white p-2 px-4 hover:bg-amarillo-pm">
                     Salir
                 </a>
             </div>
