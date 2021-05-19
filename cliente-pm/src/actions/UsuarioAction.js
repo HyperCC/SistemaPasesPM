feed904da0e2fe39a79d51ce5ff6dbb4938f613e
HyperCC <charliecondorcet@gmail.com>
2021-05-19 13:50:38 -0400

    feat(c-actions): add interaccion login

--- cliente-pm/src/actions/UsuarioAction.js
+++ cliente-pm/src/actions/UsuarioAction.js
@@ -1,8 +1,19 @@
 import ClienteHttp from '../serviciosWeb/ClienteHttp';
 
+// registro de usuarios
 export const registrarUsuario = usuario => {
     return new Promise((resolve, eject) => {
         ClienteHttp.post('/Usuario/registrar', usuario)
+            .then(response => {
+                resolve(response);
+            });
+    });
+};
+
+// login
+export const loginUsuario = credenciales => {
+    return new Promise((resolve, eject) => {
+        ClienteHttp.post('/Usuario/login', credenciales)
             .then(response => {
                 resolve(response);
             });
