a690de60b7c213948dbb234c79b4d886adab9156
Matías Quezada <matquez@gmail.com>
2021-07-05 13:21:38 -0400

    update(p-seederTipoDoc): fix seeder agregando los tipo de documento ya especificados.

--- Persistencia/Seeders/TipoDocumentoSeeder.cs
+++ Persistencia/Seeders/TipoDocumentoSeeder.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Dominio.Entidades;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -13,11 +14,48 @@ namespace Persistencia.Seeders
             var existenTipoDoc = await context.TipoDocumento.AnyAsync();
             if (!existenTipoDoc)
             {
-                string[] tipoDocumetos = { "ADMIN", 
-                    "HSEQ", 
-                    "JEFEO_PERACIONES", 
-                    "CONTACTO", 
-                    "OPIP" };
+                string[] tipoDocumetos = { 
+                    "CONTRATO DE TRABAJO",
+                    "ANEXO CONTRATO",
+                    "RIOHS",
+                    "ODI",
+                    "EPP",
+                    "BASICO",
+                    "ALTURA FISICA",
+                    "ESPACIOS CONFINADOS",
+                    "PSICOSENSOMETRICO",
+                    "SOLADOR CALIFICADO",
+                    "TRABAJO EN ALTURA",
+                    "OPERADOR EQUIPO MOVIL",
+                    "RIGGER",
+                    "OTROS"
+                };
+                bool[] obligatoriedad =
+                {
+                    true,false,true,true,true,false,false,false,false,false,false,false,false,false
+                };
+
+                for(int index = 0; index < tipoDocumetos.Length; index++)
+                {
+                    var searchTipo = await context.TipoDocumento.FirstOrDefaultAsync(t => t.Titulo == tipoDocumetos[index]);
+
+                    if (searchTipo == null)
+                    {
+                        TipoDocumento nuevoTipo = new TipoDocumento
+                        {
+                            TipoDocumentoId = new Guid(),
+                            Titulo = tipoDocumetos[index],
+                            Obligatoriedad = obligatoriedad[index]
+                        };
+                        await context.TipoDocumento.AddAsync(nuevoTipo);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"YA EXISTE EL TIPO DE DOCUMENTO {tipoDocumetos[index]}");
+                    }
+                }
+
+                await context.SaveChangesAsync();
 
             }
             else
